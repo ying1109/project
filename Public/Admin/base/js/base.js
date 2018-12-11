ea7d2f21f0d6dd39224e5e4106f1139297430cf3
ying1109 <921172520@qq.com>
2018-12-12 01:20:40 +0800

    update

--- Public/Admin/base/js/base.js
+++ Public/Admin/base/js/base.js
@@ -34,12 +34,3 @@ $(document).ready(function () {
         }
     });
 });
-
-/*$(document).ready(function () {
-    $('.menu_above li a').click(function () {
-        var parent = $(this).parent().parent();
-        parent.find('li a').css('color', '#337AB7');
-        $(this).css({'color': '#FF0000'});
-
-    })
-})*/
