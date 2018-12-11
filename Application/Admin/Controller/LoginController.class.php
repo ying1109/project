db4c0f0056d7576e0f12f3e1dc7f4b2a2f6d0387
ying1109 <44599811+ying1109@users.noreply.github.com>
2018-12-11 13:48:02 +0800

    Update LoginController.class.php

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -46,32 +46,6 @@ class LoginController extends Controller {
         }
     }
 
-    public function ajax_login111(){
-        $account    = $_POST['account'] ? $_POST['account'] : '';
-        $password   = $_POST['password'] ? $_POST['password'] : '';
-        $code_input = $_POST['code_input'] ? $_POST['code_input'] : '';
-        $code_admin = $_POST['code_admin'] ? $_POST['code_admin'] : '';
-        //$time = time();
-        if( $account == '' || $password == '' || $code_input == ''){
-            $this->ajaxReturn(array('res'=>0,'msg'=>'账号、密码、验证码不能为空！'));
-        }else{
-            if ($code_input == $code_admin) {
-                $info = M('Admin')->where(array('username'=>$account,'password'=>$password))->find();
-                if( !$info ){
-                    $this->ajaxReturn(array('res'=>0,'msg'=>'账号或密码错误'));
-                }else{
-                    //确认登录信息：保存cookie/session
-                    cookie('admin_id',$info['admin_id']);
-                    session('admin_info',$info);
-
-                    $this->ajaxReturn($info);
-                    //$this->ajaxReturn(array('res'=>1,'info'=>$info));
-                }
-            } else {
-                $this->ajaxReturn(array('res'=>0,'msg'=>'验证码错误！'));
-            }
-        }
-    }
     //退出登录
     public function loginOut(){
         $map['admin_id'] = $_COOKIE['admin_id'];
