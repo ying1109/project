3d323ea6ba1c86bf69c649dc3bb171d8f7c974eb
yiing1109 <921172520@qq.com>
2019-08-16 17:17:26 +0800

    submit

--- Application/Admin/Controller/BaseController.class.php
+++ Application/Admin/Controller/BaseController.class.php
@@ -8,12 +8,12 @@ header('Content-type:text/html; charset=utf-8');
 class BaseController extends Controller {
     public function _initialize(){
         //判断是否登录
-        $admin_id = $_COOKIE["id"];
-        if(!$admin_id){
-            $this->error('您尚未登陆，正在跳转至登陆页面', U('Login/login'));
-        }
-
-        $admin_info = D('Admin')->info(array('id'=>$admin_id));
-        $this->assign('admin', $admin_info);
+        // $admin_id = $_COOKIE["id"];
+        // if(!$admin_id){
+        //     $this->error('您尚未登陆，正在跳转至登陆页面', U('Login/login'));
+        // }
+        //
+        // $admin_info = D('Admin')->info(array('id'=>$admin_id));
+        // $this->assign('admin', $admin_info);
     }
 }
